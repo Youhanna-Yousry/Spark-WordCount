af6b5919c66e59b6a9905e0ab600b0c7ba64ba04
Youhanna-Yousry <youhanna.yousry.2580@gmail.com>
2024-04-07 16:18:27 +0200

    reformated WordCount class

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -19,21 +19,21 @@ public class WordCount {
         // Create a Java Spark Context.
         SparkConf conf = new SparkConf().setMaster("local").setAppName("wordCount");
 
-		try(JavaSparkContext sc = new JavaSparkContext(conf)) {
-			// Load our input data.
-			JavaRDD<String> input = sc.textFile(inputFile);
+        try (JavaSparkContext sc = new JavaSparkContext(conf)) {
+            // Load our input data.
+            JavaRDD<String> input = sc.textFile(inputFile);
 
-			// Split up into words.
-			JavaRDD<String> words = input.flatMap((x) -> Arrays.asList(x.split(" ")).iterator());
+            // Split up into words.
+            JavaRDD<String> words = input.flatMap((x) -> Arrays.asList(x.split(" ")).iterator());
 
-			// Map each word to a (word, 1) pair
-			JavaPairRDD<String, Integer> ones = words.mapToPair((x) -> new Tuple2<>(x, 1));
+            // Map each word to a (word, 1) pair
+            JavaPairRDD<String, Integer> ones = words.mapToPair((x) -> new Tuple2<>(x, 1));
 
-			// ReduceByKey to count the occurrences of each word
-			JavaPairRDD<String, Integer> counts = ones.reduceByKey(Integer::sum);
+            // ReduceByKey to count the occurrences of each word
+            JavaPairRDD<String, Integer> counts = ones.reduceByKey(Integer::sum);
 
-			// Save the word count back out to a text file, causing evaluation.
-        	counts.saveAsTextFile(outputFile);
-		}
+            // Save the word count back out to a text file, causing evaluation.
+            counts.saveAsTextFile(outputFile);
+        }
     }
 }
